0f8be3bc9ea9f5befadfc88c928deb4e8f7533bd
luml <1520622848@qq.com>
2021-08-31 22:12:45 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Basic from "../views/Basic.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Basic",
+    component: Basic,
   },
   {
     path: "/about",
---
+++ src/views/Basic.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition appear @before-enter="beforeEnter" @enter="enter" :css="false">
+    <div class="crad"></div>
+  </transition>
+</template>
+
+<script>
+// @ is an alias to /src
+import gsap from "gsap";
+
+export default {
+  name: "Basic",
+  components: {},
+  methods: {
+    beforeEnter(el) {
+      el.style.opacity = 0;
+      el.style.transform = "scale(0,0)";
+    },
+    enter(el, done) {
+      gsap.to(el, {
+        duration: 1,
+        opacity: 1,
+        scale: 1,
+        onComplete: done,
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.crad {
+  width: 50px;
+  height: 50px;
+  background: teal;
+}
+</style>
